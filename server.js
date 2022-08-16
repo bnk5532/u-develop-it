26c10314208754b07762152160d9a7edc2372162
Brian King <bnk5532@gmail.com>
2022-08-16 17:27:59 -0400

    parties table added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
+    password: 'Darwin1882',
     database: 'election'
   },
   console.log('Connected to the election database.')
